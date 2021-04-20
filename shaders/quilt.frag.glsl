af445d102dbd0e62e035c7058ede5412b67714be
Vincent Huang <vincenthuang75025@yahoo.com>
2021-04-19 17:31:56 -0700

    quilt colors

--- shaders/quilt.frag.glsl
+++ shaders/quilt.frag.glsl
@@ -12,22 +12,24 @@ void main() {
     color.z += abs(sin(time));
 
     // 1. Uncomment these lines to draw triangles
-    // vec2 squareCoord = 20.0 * gl_FragCoord.xy / resolution.y + vec2(time);
-    // vec2 loc = fract(squareCoord);
-    // color = vec3(smoothstep(-0.05, 0.05, loc.y - loc.x));
+    vec2 squareCoord = 20.0 * gl_FragCoord.xy / resolution.y + vec2(time);
+    vec2 loc = fract(squareCoord);
+    color = vec3(smoothstep(-0.05, 0.05, loc.y - loc.x));
 
     // 2. Uncomment these lines to invert some of the triangles
-    // vec2 cell = squareCoord - loc;
-    // if (mod(2.0 * cell.x + cell.y, 5.0) == 1.0) {
-    //     color = 1.0 - color;
-    // }
+    vec2 cell = squareCoord - loc;
+    if (mod(2.0 * cell.x + cell.y, 5.0) >= seed * 5.0) {
+        color = 1.0 - color;
+    }
 
     // 3. Uncomment these lines to produce interesting colors
-    // float c = mod(3.0 * cell.x + 2.0 * cell.y, 7.0) / 7.0;
-    // color = 1.0 - (1.0 - color) * vec3(c, c, c);
+    float c = mod(3.0 * cell.x + 2.0 * cell.y, 7.0) / 7.0;
+    float d = mod(2.0 * cell.x + 3.0 * cell.y, 5.0)/5.0;
+    float e = mod(cell.x + cell.y, 2.0)/2.0;
+    color = 1.0 - (1.0 - color) * vec3(c, d, e);
 
     // 4. Uncomment to lighten the colors
-    // color = sqrt(color);
+    color = sqrt(color);
 
     gl_FragColor = vec4(color, 1.0);
 }
